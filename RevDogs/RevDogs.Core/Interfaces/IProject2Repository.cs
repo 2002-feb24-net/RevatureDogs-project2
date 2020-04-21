b6bd0f3d7cad0d49a014c226d23fec441095c6d6
abraham <Abrahambergerson@gmail.com>
2020-04-20 23:05:08 -0500

    modified IRepo

--- RevDogs/RevDogs.Core/Interfaces/IProject2Repository.cs
+++ RevDogs/RevDogs.Core/Interfaces/IProject2Repository.cs
@@ -1,22 +1,33 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using RevDogs.Core.Model;
 
 namespace RevDogs.Core.Interfaces
 {
     public interface IProject2Repository
     {
-        void PutDogs(Dogs dogs);
-        void GetDogs(Dogs dogs);
-        void PostDogs(Dogs dogs);
-        void DeleteDogs(Dogs dogs);
-        void GetUsers(Users users);
-        void PutUsers(Users users);
-        void PostUsers(Users users);
-        void DeleteUsers(Users users);
+        Task<Users> GetUsers(int id);
+        Task<Users> PutUsers(Users users);
+        Task<Users> PostUsers(Users users);
+        Task<Users> DeleteUsers(int id);
 
+        // gets all dogs
+        Task<IEnumerable<Dogs>> GetDogs();
+        // gets dog by id
+        Task<Dogs> GetDogs(int id);
+        // gets dog by User id
+        Task<IEnumerable<Dogs>> GetUserDogs(int Userid);
+        Task<Dogs> PutDogs(Dogs dogs);
+        Task<Dogs> PostDogs(Dogs dogs);
+        Task<Dogs> DeleteDogs(int id);
 
+        // gets trick progress based off pet ID
+        Task<IEnumerable<TricksProgress>> GetTricksProgress(int id);
 
+        Task<TricksProgress> PutTricksProgress(TricksProgress tricksProgress);
+
+        Task<TricksProgress> PostTricksProgress(TricksProgress tricksProgress);
     }
 }
