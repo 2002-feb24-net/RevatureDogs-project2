127d1499b6993a3dc68a3993b2c0b557b0c79ae0
Angelica Velez <anvelez26@gmail.com>
2020-04-23 22:28:31 -0500

    user controller testing

--- RevDogs/RevDogs.Testing/UsersControllerTests.cs
+++ RevDogs/RevDogs.Testing/UsersControllerTests.cs
@@ -9,27 +9,37 @@ using RevDogs.Core.Interfaces;
 using RevDogsApi.Controllers;
 using RevDogsApi.Models;
 using RevDogs.Core.Model;
-
+using FluentAssertions;
+using RevDogs.DataAccess.Model;
+using RevDogs.DataAccess;
 
 namespace RevDogs.Testing
 {
     public class UsersControllerTests
     {
-        private readonly UsersController _controller;
-        public  UsersControllerTests()
-        {
-            var mockRepository = new Mock<IProject2Repository>();
-            var user = new RevDogs.Core.Model.Users
+
+
+            public void GetUsers()
             {
+            //  var mockRepository = new Mock<IProject2Repository>();
+            //  var user = new RevDogs.Core.Model.Users
+             var listOfUsers = new Core.Model.Users();
+             listOfUsers = new Core.Model.Users
+             {
                 Id = 1,
                 UserName = "TestUserName",
                 FirstName = "TestFirstName",
                 LastName = "TestLastName",
-                Score = 1
-            };
+                Score = 1,
+             };
 
-        }
 
+            var a = Mapper.MapUsers(listOfUsers);
+            Mock<IProject2Repository> mockIProject2Repository = new Mock<IProject2Repository>();
+            mockIProject2Repository.Setup(x => x.GetUsersAsync()).Verifiable();
+            var usersController = new UsersController(mockIProject2Repository.Object);
+            usersController.Should().NotBeNull();
+            }
 
     }
 }
